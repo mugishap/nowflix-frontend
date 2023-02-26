46558e7fa09fbe0dca72cb4c9bdb2a58e82e394d
mugishap <precieuxmugisha@gmail.com>
2023-02-26 18:43:15 +0200

    [fix]: Error pop on search

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -29,7 +29,7 @@ const Search = () => {
     }, [movies])
 
     useEffect(() => {
-        toast.error(error)
+        if (error) toast.error(error)
     }, [error])
 
     const getPageNumbers = () => {
